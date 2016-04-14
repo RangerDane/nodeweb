b7ec5e1fe6b53841350a3c92fd44cc48489812fe
Michael Shane <msg@stargh.com>
2016-04-14 16:52:50 -0700

    update distance clipping

--- nodeweb.js
+++ nodeweb.js
@@ -1,15 +1,31 @@
 (function(){
-  var _X = 600;
-  var _Y = 600;
+  // var _X = 600;
+  // var _Y = 600;
 
-  _X = window.innerWidth;
-  _Y = window.innerHeight;
+  var _X = window.innerWidth;
+  var _Y = window.innerHeight;
 
   var _Z = ( _X + _Y ) / 2;
 
+  var _COLORS = [ [165,13,46],
+              [186,61,90],
+              [221,202,137],
+              [237,234,213],
+
+              [247,165,192],
+              [252,65,171],
+              [250,44,4],
+              [117,33,57],
+              [130,36,227]];
+
+  _RANDS = [];
+  for (var i = 0; i < 50; i++) {
+    _RANDS.push( Math.random() );
+  }
+
   var Node = function( ctx ) {
     var x = Math.random() * _X;
-    var y = Math.random() * _X;
+    var y = Math.random() * _Y;
     var z = Math.random() * _Z;
     var xvel = Math.random() * 4 - 2;
     var yvel = Math.random() * 4 - 2;
@@ -77,8 +93,11 @@
     this.pos[2] += this.vel[2];
 
     // fake 3d
-    this.offx = ( this.pos[0] + ( ( (_X / 2) - this.pos[0] ) * ( 0.3 * ( this.pos[2]/_X ) ) ) );
-    this.offy = ( this.pos[1] + ( ( (_Y / 2) - this.pos[1] ) * ( 0.3 * ( this.pos[2]/_Y ) ) ) );
+    // this.offx = ( this.pos[0] + ( ( (_X / 2) - this.pos[0] ) * ( 0.3 * ( this.pos[2]/_X ) ) ) );
+    // this.offy = ( this.pos[1] + ( ( (_Y / 2) - this.pos[1] ) * ( 0.3 * ( this.pos[2]/_Y ) ) ) );
+
+    this.offx = this.pos[0];
+    this.offy = this.pos[1];
 
     this.testOutOfBounds();
   }
@@ -91,7 +110,7 @@
   Web.prototype.start = function() {
     // this.ctx.fillStyle = "blue";
     // this.ctx.fillRect(20,20,150,100);
-    for (var i = 0; i < 40; i++) {
+    for (var i = 0; i < 50; i++) {
       this.nodes.push( new Node( this.ctx ) );
     }
     // kick off the show
@@ -100,7 +119,7 @@
 
   Web.prototype.render = function() {
     this.ctx.clearRect(0,0,_X,_Y);
-    var dist, x1, y1, z1, x2, y2, z2, color, closest;
+    var dist, x1, y1, z1, x2, y2, z2, r, g, b, alpha, closest;
     for (var f = 0; f < this.nodes.length; f++) {
       x1 = this.nodes[f].pos[0];
       y1 = this.nodes[f].pos[1];
@@ -110,10 +129,14 @@
         y2 = this.nodes[s].pos[1];
         z2 = this.nodes[s].pos[2];
         dist = Math.sqrt( Math.pow((x1-x2),2) + Math.pow((y1-y2),2) + Math.pow((z1-z2),2) );
-        if ( dist < _Z * 2 ) {
-          color = 1/Math.pow(dist,2) * 30 * _Z;
-          this.ctx.lineWidth = 1;
-          this.ctx.strokeStyle = "rgba(255,255,255," + color + ")";
+        if ( dist < _Z ) {
+          color = _COLORS[ (f + s) % _COLORS.length ];
+          alpha = 1/Math.pow(dist,2) * 35 * _Z;
+          this.ctx.lineWidth = Math.min( Math.max( alpha, 1 ), 2);
+          r = color[0];
+          g = color[1];
+          b = color[2]
+          this.ctx.strokeStyle = "rgba(" + r +"," + g + "," + b + "," + alpha + ")";
           this.ctx.beginPath();
           this.ctx.moveTo(this.nodes[f].offx,this.nodes[f].offy);
           this.ctx.lineTo(this.nodes[s].offx,this.nodes[s].offy);
@@ -124,7 +147,22 @@
       this.nodes[f].move();
     }
     requestAnimationFrame( this.render.bind(this) );
-  }
+  };
+
+  Web.prototype.collectNodes = function(x,y) {
+    // var i = 0;
+    // var intensity;
+    this.nodes.forEach( function( node ) {
+      // inverse square law
+      // intensity = 1000000 / ( Math.pow( x - node.pos[0], 2 ) + Math.pow( y - node.pos[1], 2 ) );
+      // node.vel[0] = ( x - node.pos[0] ) * ( _RANDS[ i % _RANDS.length ] * (Math.min( 50, intensity ) + i) ) / ( _X );
+      // node.vel[1] = ( y - node.pos[1] ) * ( _RANDS[ i % _RANDS.length ] * (Math.min( 50, intensity ) + i) ) / ( _Y );
+      // console.log(intensity);
+      // i++;
+      node.vel[0] = Math.random() * 4 - 2;
+      node.vel[1] = Math.random() * 4 - 2;
+    });
+  };
 
   document.addEventListener( "DOMContentLoaded", function() {
     var canvas = document.getElementById("canvas");
@@ -134,10 +172,12 @@
 
     var web = new Web( ctx );
     web.start();
+
+    document.addEventListener("click", function(e) { web.collectNodes(e.pageX, e.pageY) } );
+    // document.addEventListener("mousemove", function(e) { web.collectNodes(e.pageX, e.pageY) } );
   });
 
   var resizeWindow = function() {
-
     _X = window.innerWidth;
     _Y = window.innerHeight;
     _Z = ( _X + _Y ) / 2;
